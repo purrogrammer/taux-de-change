6561881f0cab316172f7f0956a3eaee78b905cf8
Marguerite Kenedy <reach.marguerite@gmail.com>
2020-02-21 14:35:22 -0800

    fix currency change function

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -4,12 +4,14 @@ export default function CurrencyRow(props) {
     const {
         currencyOptions,
         selectedCurrency,
-        onChangeCurrency
+        onChangeCurrency,
+        onChangeAmount,
+        amount
     } = props
     return (
         <div>
-            <input type="number" className="input" />
-            <select value={selectedCurrency}>
+            <input type="number" className="input" value={amount} onChange={onChangeAmount} />
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
                 {currencyOptions.map(option => (   
                 
                  <option key={option} value={option}>{option} </option>
